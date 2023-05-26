89219a4da152a990dd0296ea04ab46dd4f9f5982
63Paulo <fernandespaul548@gmail.com>
2023-05-26 13:53:13 +0200

    Portfolio

--- js/NavbarMobile.js
+++ js/NavbarMobile.js
@@ -1,3 +1,4 @@
+// Code pour que quand on appuis sur le bouton, la navbar s'affiche
 function toggleNavbar() {
     const navbar = document.querySelector('.navbar');
     navbar.classList.toggle('collapsed');
