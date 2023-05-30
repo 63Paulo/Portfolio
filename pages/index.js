d7949f71687cef957b1a0e92fe20e2489161423f
63Paulo <fernandespaul548@gmail.com>
2023-05-30 15:31:56 +0200

    Conversion NextJS

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/style.css';
 
 export default function Home() {
   const handleSubmit = (event) => {
