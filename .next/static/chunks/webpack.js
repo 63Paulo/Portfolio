160b9ef3df27037eb74087cb233d76672848f83c
63Paulo <fernandespaul548@gmail.com>
2023-06-01 10:02:45 +0200

    Installation tailwind css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b4344842e69439a9"; }
+/******/ 		__webpack_require__.h = function() { return "7a8161124be4487c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
