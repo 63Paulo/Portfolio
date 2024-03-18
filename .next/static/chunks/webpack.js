8df1044956da1a97bd36ab70b2b5403006b357d7
Paul Fernandes <88905397+63Paulo@users.noreply.github.com>
2024-03-18 13:08:20 +0100

    Modification

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dcba1e0448a059f7"; }
+/******/ 		__webpack_require__.h = function() { return "0e6b5f4fae398577"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- Components/Parcours/Parcours.js
+++ Components/Parcours/Parcours.js
@@ -38,8 +38,8 @@ const Parcours = () => {
         <>
           <section className="parcours" id="parcours">
             <h1 className="parcoursH1 homepageH1">MON PARCOURS</h1>
-            <h2 className="parcoursH2">Je suis un étudiant en première année de BTS SIO avec de grandes ambitions. Je souhaite obtenir 
-                une licence en informatique puis un master pour approfondir mes connaissances et compétences. 
+            <h2 className="parcoursH2">Je suis un étudiant en deuxième année de BTS SIO avec de grandes ambitions. Je souhaite obtenir 
+                une licence en informatique puis potentiellement un master pour approfondir mes connaissances et compétences. 
                 Mon parcours académique reflète mon désir de développement professionnel et ma passion pour la technologie. 
                 Je suis motivé à travailler dur pour atteindre mes objectifs et contribuer au progrès technologique.
             </h2>
